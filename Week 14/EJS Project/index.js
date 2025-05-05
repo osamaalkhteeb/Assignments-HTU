f9d706d7690be39a8dd97466aaa9b6a1088a436a
osamaalkhatib <osama200181@gmail.com>
2025-05-06 00:29:03 +0300

    ejs project edit

--- Week 14/EJS Project/index.js
+++ Week 14/EJS Project/index.js
@@ -11,7 +11,7 @@ const port = 3000;
 
 class Post{
   constructor(title,content){
-  this.id = nextId++;
+  this.id = (nextId++).toString();
   this.title = title;
   this.content = content;
   }};
@@ -23,14 +23,33 @@ app.get("/", (req, res) => {
   res.render("index.ejs",{posts});
 });
 
+app.get("/new", (req, res) => {
+  res.render("new.ejs");
+});
 app.post("/new", (req, res) => {
   const title = req.body.title;
   const content = req.body.content;
   const post = new Post(title,content);
   posts.push(post);
-  res.render("index.ejs",{posts});
+  res.redirect("/");
 });
 
+app.get("/edit/:id",(req,res)=>{
+  const post = posts.find((p) => p.id === req.params.id);
+  if(!post) return res.redirect("/");
+  res.render("edit.ejs",{post});
+})
+
+app.post("/edit/:id",(req,res) =>{
+  const title = req.body.title;
+  const content = req.body.content;
+  posts = posts.map((p) => (p.id === req.params.id ? { ...p, title, content } : p));
+  res.redirect("/");
+})
+app.post("/delete/:id",(req,res)=>{
+  posts = posts.filter((p)=> p.id !== req.params.id)
+  res.redirect("/");
+})
 app.listen(port, () => {});
 
 
